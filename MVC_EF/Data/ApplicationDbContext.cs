33f18fb505173221e8e60a2f13fc103aed9287eb
Julio Damasceno (BEYONDSOFT CONSULTING INC) <v-jdamasceno@microsoft.com>
2024-11-28 23:17:16 -0300

    Add project files.

---
+++ MVC_EF/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVC_EF.Models;
+
+namespace MVC_EF.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MVC_EF.Models.Person> Person { get; set; } = default!;
+    }
+}
---
+++ MVC_EF/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_EF.Models
+{
+    public class Person
+    {
+        [Key]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+    }
+}
